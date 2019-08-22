0d162a33be5a47e047b905e696eb90a8837cf915
XieGuochao <117010307@link.cuhk.edu.cn>
2019-08-22 22:59:42 +0800

    open browser

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-import server
+import server, os
+from multiprocessing import Process
 
 app = server.create_app()
+def open_browser():
+    if os.name == 'nt':
+        os.system("start http://localhost:5000/")
+    else:
+        os.system("python -m webbrowser \"http://localhost:5000/\"")
+
+def start_app():
+    app.run(host="0.0.0.0", port=5000)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    open_browser()
+    start_app()
