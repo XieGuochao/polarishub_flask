8b173d4703c1a2218636effaeb1217477afc0d32
xieguochao <117010307@link.cuhk.edu.cn>
2019-09-01 22:49:22 +0800

    fix Chinese

--- polarishub_flask/server/myqrcode.py
+++ polarishub_flask/server/myqrcode.py
@@ -1,19 +1,34 @@
-from MyQR import myqr
+# from MyQR import myqr
+import qrcode
 import os
 from polarishub_flask.server.parser import printv
 
+# def generateCode(url, filename = "qrcode.png"):
+#     version, level, qr_name = myqr.run(
+#         url,
+#         version = 1,
+#         level = 'H',
+#         picture = None,
+#         colorized = False,
+#         contrast = 1.0,
+#         brightness = 1.0,
+#         save_name = filename,
+#         save_dir = os.path.join(os.getcwd(), 'temp')
+#     )
+#     printv(qr_name)
+#     return qr_name, '/' + qr_name[qr_name.find("temp"):].replace("\\", "/")
+
 def generateCode(url, filename = "qrcode.png"):
-    version, level, qr_name = myqr.run(
-        url,
-        version = 1,
-        level = 'H',
-        picture = None,
-        colorized = False,
-        contrast = 1.0,
-        brightness = 1.0,
-        save_name = filename,
-        save_dir = os.path.join(os.getcwd(), 'temp')
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
     )
+    qr.add_data(url)
+    qr.make(fit=True)
+    qr_name = os.path.join(os.getcwd(), 'temp', filename)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(qr_name)
     printv(qr_name)
     return qr_name, '/' + qr_name[qr_name.find("temp"):].replace("\\", "/")
-# printv(version, level, qr_name)
