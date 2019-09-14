c4a4e820e1c15cd23b3ec08bacb27d2de2148257
xieguochao <117010307@link.cuhk.edu.cn>
2019-09-14 19:09:11 +0800

    update 0.1.16

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIRED = [
 
 setuptools.setup(
     name="polarishub_flask",
-    version="0.1.15",
+    version="0.1.16",
     author="Guochao Xie & Senyue Hao",
     author_email="guochaoxie@link.cuhk.edu.cn",
     description="PolarisHub Flask version",
