167a022112c63246a72d7d29fcd8b4faf80c617f
XieGuochao <117010307@link.cuhk.edu.cn>
2019-09-14 19:04:02 +0800

    fix process of windows

--- polarishub_flask/app.py
+++ polarishub_flask/app.py
@@ -16,8 +16,10 @@ def start_app():
     app.run(host = "0.0.0.0", port = 5000, threaded = True)
 
 def main():
-    Process(target=lambda _:open_browser(), args=(None, )).start()
-    # open_browser()
+    if sys.platform == "win32":
+        open_browser()
+    else:
+        Process(target=lambda _:open_browser(), args=(None, )).start()
     start_app()
 
 if __name__ == "__main__":
